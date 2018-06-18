c4684f8d2cfcc872f6d595a7a25e9e37e9a98f05
LekcRg <lekcrg@ya.ru>
2018-06-18 11:22:50 +0300

    works page

--- src/assets/styles/variables.js
+++ src/assets/styles/variables.js
@@ -1,7 +1,7 @@
 module.exports = {
-  'green-hover': "#4e8839",
-  green: '#6c9c5a',
-  "text-color": "#fff",
-  grey: "#566358",
-  black: '#373e42'
+	'green-hover': "#4e8839",
+	green: '#6c9c5a',
+	"text-color": "#fff",
+	grey: "#566358",
+	black: '#373e42'
 };
