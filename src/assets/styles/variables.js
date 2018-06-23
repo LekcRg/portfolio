d1dc6621d09595f05971aa2c6bf62cd64ca64034
LekcRg <lekcrg@ya.ru>
2018-06-23 16:42:04 +0300

    fix HW

--- src/assets/styles/variables.js
+++ src/assets/styles/variables.js
@@ -3,5 +3,15 @@ module.exports = {
 	green: '#6c9c5a',
 	"text-color": "#fff",
 	grey: "#566358",
-	black: '#373e42'
+	black: '#373e42',
+	'hamburger-padding-x': '15px',
+	'hamburger-padding-y': '15px',
+	'hamburger-layer-width': '40px',
+	'hamburger-layer-height': '4px',
+	'hamburger-layer-spacing': '6px',
+	'hamburger-layer-color': '#fff',
+	'hamburger-layer-border-radius': '4px',
+	'hamburger-hover-opacity': '0.7',
+	'hamburger-hover-transition-duration': '0.15',
+	'hamburger-hover-transition-timing-function': 'linear'
 };
