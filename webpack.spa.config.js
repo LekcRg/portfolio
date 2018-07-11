74b56265b0acd061e092c7c4b461229b5d71c366
LekcRg <lekcrg@ya.ru>
2018-07-11 05:59:06 +0300

    nope, it was not last commit

--- webpack.spa.config.js
+++ webpack.spa.config.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist/admin"),
     filename: "[name].bundle.js",
-    publicPath: "/admin"
+    publicPath: "/admin/"
   },
   module: {
     rules: [
